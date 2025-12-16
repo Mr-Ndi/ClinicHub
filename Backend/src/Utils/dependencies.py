b908e236888a6807b3b2f1abbdcde4e830de0933
Mr-Ndi <onlythenotes@gmail.com>
2025-12-16 09:17:02 +0200

    Feature: Require admin or doctor

--- Backend/src/Utils/dependencies.py
+++ Backend/src/Utils/dependencies.py
@@ -1,3 +1,4 @@
+
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from src.Utils.jwtGenerator import decode_access_token
@@ -13,4 +14,9 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
 def require_admin(user=Depends(get_current_user)):
     if user.get("role") != "admin":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Admins only")
+    return user
+
+def require_admin_or_doctor(user=Depends(get_current_user)):
+    if user.get("role") not in ("admin", "doctor"):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Admins or doctors only")
     return user
